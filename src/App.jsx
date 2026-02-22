b8de744907ff17a19a2a56e78457e44fdf1ca47b
Zachary Baur <zachary.baur@gmail.com>
2026-02-22 18:22:44 +0000

    Created a new component that displays an unordered list of all the components of my skateboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 
 const setup = {
-  deck: 'WKND - Nikolai Piombo Pro Model (8.25)'
-  trucks: 'Independent - 141 Hollow'
-  wheels: 'Spitfire - Formula Four Classic (56mm, 96)'
-  bearings: 'Bones - Red Bearings'
+  deck: 'WKND - Nikolai Piombo Pro Model (8.25)',
+  trucks: 'Independent - 141 Hollow',
+  wheels: 'Spitfire - Formula Four Classic (56mm, 96)',
+  bearings: 'Bones - Red Bearings',
   griptape: 'Mob - Grip Tape'
 };
+
+function CurrentSetup() {
+  return (
+    <div>
+      <h2>Current Skateboard Setup:</h2>
+      <ul>
+        <li><strong>Deck:</strong> {setup.deck}</li>
+        <li><strong>Trucks:</strong> {setup.trucks}</li>
+        <li><strong>Wheels:</strong> {setup.wheels}</li>
+        <li><strong>Bearings:</strong> {setup.bearings}</li>
+        <li><strong>Griptape:</strong> {setup.griptape}</li>
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -14,6 +29,7 @@ function App() {
       <h1 className="title">
         This site is dedicated to keepign track of Zachary Baur's current skateboard setup!
       </h1>
+      <CurrentSetup />
     </>
   )
 }
