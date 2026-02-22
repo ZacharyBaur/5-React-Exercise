e865ba31bca8e69a4959366cd8c165dd0fd0c61a
Zachary Baur <zachary.baur@gmail.com>
2026-02-22 18:08:13 +0000

    got rid of all vite + react template code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        This site was made in a matter of seconds using Vite and React. Isn't that crazy!
-      </p>
+      <h1 className="title">
+        This site is dedicated to keepign track of Zachary Baur's current skateboard setup!
+      </h1>
     </>
   )
 }
